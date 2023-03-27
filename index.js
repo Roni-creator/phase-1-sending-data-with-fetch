12412e04e18c0cceeac7b5e6142c4ed5d50c167f
veronicah <mbuviveronicah3@gmail.com>
2023-03-27 19:43:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+function submitData(Name,Email)
+const users = {
+    name:Name,
+    email:Email,
+};
+
+return fetch( 'http://localhost:3000/users',{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(users),
+  })
+  
+  .then( function ( response ) {
+    return response.json()
+  } )
+  .then( function ( object ) {
+    document.body.innerHTML = object[ "id" ]
+  } )
+  .catch( function ( error ) {
+    document.body.innerHTML = error.message
+  } )
+
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+ 
